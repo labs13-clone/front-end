765f556cb77138c97f63a3be18a5472344d7c068
Lidii <lidiiany92@gmail.com>
2019-05-31 16:29:40 -0400

    Created the base for user card view

---
+++ src/Components/UserView/UserView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserInfoCard from '../UserView/UserInfoCard';
+
+const UserView = () => {
+  return (
+    <div >
+        <UserInfoCard />
+    </div>
+  );
+}
+
+export default UserView;
