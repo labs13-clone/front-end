2c0b61fb37ceba8cb219ee555c8081f97d5bc537
chasefulks <chase.fulks@gmail.com>
2019-05-31 15:01:01 -0500

    landingpagebeginings

---
+++ src/Components/LandingPage/LandingPageContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LandingPageContent from "./LandingPageContent.js"
+
+const LandingPageContainer = props =>{
+    return(
+<div className="landingContainer">
+<a href="//www.reactjs.org ">
+        <LandingPageContent />
+      </a>
+
+</div>
+    );
+};
+
+
+export default LandingPageContainer
---
+++ src/Components/LandingPage/LandingPageContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./LandingPage.css";
+
+const LandingPageContent = props => {
+    return (
+<div classname="landingContent">
+<h1>Clone Coding</h1>
+<button>Sign In</button>
+<button>Register</button>
+<h2>Attempt Code Challenges</h2>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+</div>
+    );
+};
+export default LandingPageContent;
