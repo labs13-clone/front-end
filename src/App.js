24db1eb03dbe2e3a33b21d38d29903fca62ffe44
chasefulks <chase.fulks@gmail.com>
2019-05-31 14:38:45 -0500

    filenames

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import HeaderContainer from "./Components/HeaderComponents/HeaderContainer.js";
+import LandingPage from "./Components/LandingPage/LandingPage"
 function App() {
   return (
     <div className="App">
