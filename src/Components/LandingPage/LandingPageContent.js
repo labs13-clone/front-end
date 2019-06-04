67df25b309a96f2097fe401f0aa406f703fe7808
chasefulks <chase.fulks@gmail.com>
2019-06-04 14:49:25 -0500

    landingpagestyle

--- src/Components/LandingPage/LandingPageContent.js
+++ src/Components/LandingPage/LandingPageContent.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./LandingPage.css";
+require ('./LandingPage.css')
 
 const LandingPageContent = props => {
   return (
-    <div classname="landingContent">
+    <div className="landingContent">
       <header>
         <h1>Clone Coding</h1>
         <div>
@@ -11,16 +11,20 @@ const LandingPageContent = props => {
         <button>Register</button>
         </div>
       </header>
-      <div classname="topContent">
+      <div className="topContent">
       <h2>Achieve mastery through challenge</h2>
+      <div className="topText">
       <p>
        Clone Coding is the best platform to help you enhance your skills, expand your knowledge, and prepare for technical interviews.
       </p>
-    
+    <div className="topButtons">
       <button>Continue with Google</button>
       <button>Continue with Github</button>
       </div>
+      </div>
+      </div>
       <div className="body">
+        <div>
         <h2>Attempt Code Challenges</h2>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -30,6 +34,7 @@ const LandingPageContent = props => {
           five centuries, but also the leap into electronic typesetting,
           remaining essentially unchanged.
         </p>
+        <div>
         <h2>Create Challenges and Tests</h2>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -39,6 +44,8 @@ const LandingPageContent = props => {
           five centuries, but also the leap into electronic typesetting,
           remaining essentially unchanged.
         </p>
+        </div>
+        <div>
         <h2>Attempt Code Challenges</h2>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -48,7 +55,9 @@ const LandingPageContent = props => {
           five centuries, but also the leap into electronic typesetting,
           remaining essentially unchanged.
         </p>
+        </div>
       </div>
+    </div>
     </div>
   );
 };
