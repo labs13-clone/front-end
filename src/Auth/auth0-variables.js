2d80b6f7354a2f2c56e56eb9f906ffa5b3923e72
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-06-07 01:10:20 -0500

    tied environment variables into auth0-variables file

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,7 @@
+export const AUTH_CONFIG = {
+    domain: process.env.DOMAIN,
+    clientId: process.env.CLIENTID,
+    callbackUrl: process.env.DEV_CALLBACKURL,
+    audience: process.env.AUDIENCE,
+    returnTo: process.env.DEV_RETURNTO
+}
