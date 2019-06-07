14325d0f76bda97e7e99250ede0a0f359defdf5f
Lidii <lidiiany92@gmail.com>
2019-06-07 13:14:19 -0400

    Deleted console.logs

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App(props) {
   //Is the user authenticated?
   const { isAuthenticated } = props.auth;
 
-  console.log(props.auth.isAuthenticated());
+  
 
   return (
     <div className="App">
--- src/Auth/ProtectedRoute.js
+++ src/Auth/ProtectedRoute.js
@@ -3,9 +3,9 @@ import {Route, Redirect} from 'react-router-dom';
 import Auth from './Auth';
 
 export default auth => ({component: Component, ...props}) => {
-    console.log(auth.isAuthenticated());
+    
     if(auth.isAuthenticated()) {
-        console.log(Component);
+        
         return <Route {...props} render={(props) => <Component {...props} />} />
 
     } else {
