facf18dbfdf5dc0ae97dfef5bd627b942cfb92de
Lidii <lidiiany92@gmail.com>
2019-06-10 20:05:24 -0400

    Getting all challenges from the backend

--- src/Components/Views/SearchChallenges/SearchChallenges.js
+++ src/Components/Views/SearchChallenges/SearchChallenges.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import CategoriesFilter from './CategoriesFilter';
 import DifficultyLevels from './DifficultyLevels';
+import ChallengesContainer from './ChallengesContainer';
 
-const SearchChallenges = () => {
+const SearchChallenges = (props) => {
+
+  console.log('props', props)
   return (
     <div >
         <CategoriesFilter />
         <DifficultyLevels />
+        <ChallengesContainer auth={props.auth}/>
     </div>
   );
 }
