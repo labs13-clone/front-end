17b1676808b73b3038f8af21012c0dd4cdb1f12d
Anubhav311 <anubhavbhambri1@gmail.com>
2019-06-12 23:27:33 +0530

    added markdown render

--- src/Components/Views/CreateChallenge/CreateChallenge.js
+++ src/Components/Views/CreateChallenge/CreateChallenge.js
@@ -1,9 +1,58 @@
 import React from 'react'
+import {ReactDOM, render} from 'react-dom'
+import MarkdownJsx from 'markdown-to-jsx';
+import ReactMarkdown from 'react-markdown';
 
-export default function CreateChallenge() {
-    return (
-        <div>
-            Add Challenge Route
-        </div>
-    )
+import Editor from '../../Shared/Editor/Editor';
+
+// render(
+//     <Markdown>
+//         # Hi
+//     </Markdown>,
+//     document.body);
+
+class CreateChallenge extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            code: ''
+        }
+    }
+
+    render() {
+        const input = '# heading \n\n * bold \n ## this is [h2](https://github.com/rexxars/react-markdown)\n\n '
+        return(
+            <div>
+                <div className="meta-container">
+                    <h3>Meta</h3>
+                    <form>
+                        <h4>Title</h4>
+                        <input/>
+                        <h4>Difficulty</h4>
+                        <input/>
+                        <h4>Tags</h4>
+                        <input/>
+                    </form>
+                </div>
+                <div className="instructions-container">
+                    <h3>instructions</h3>
+                    <p>we are going to pull a paragraph from backend and will render here.</p>
+                </div>
+                <div id="markdown">
+
+                </div>
+                <ReactMarkdown source={input} />
+                    
+                <ol>
+                    <li>bold</li>
+                </ol>
+                <div className="creation-editor-container">
+                    <Editor/>
+                    <h1>Hi</h1>
+                </div>
+            </div>
+        )
+    }
 }
+
+export default CreateChallenge;
