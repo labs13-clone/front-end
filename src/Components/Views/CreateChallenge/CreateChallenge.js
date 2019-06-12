3dc889e3d09688dfac84f30532df10b0b126c5a4
Anubhav311 <anubhavbhambri1@gmail.com>
2019-06-13 02:16:51 +0530

    Created markdown editor and render component

--- src/Components/Views/CreateChallenge/CreateChallenge.js
+++ src/Components/Views/CreateChallenge/CreateChallenge.js
@@ -1,58 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {ReactDOM, render} from 'react-dom'
 import MarkdownJsx from 'markdown-to-jsx';
 import ReactMarkdown from 'react-markdown';
 
 import Editor from '../../Shared/Editor/Editor';
+import "./CreateChallenge.css";
 
-// render(
-//     <Markdown>
-//         # Hi
-//     </Markdown>,
-//     document.body);
 
-class CreateChallenge extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            code: ''
-        }
+function CreateChallenge() {
+    let [input, setInput] = useState('')
+
+    function handleInputChange(editor, data, code){
+        setInput(code);
+        console.log(code)
     }
-
-    render() {
-        const input = '# heading \n\n * bold \n ## this is [h2](https://github.com/rexxars/react-markdown)\n\n '
-        return(
-            <div>
-                <div className="meta-container">
-                    <h3>Meta</h3>
-                    <form>
-                        <h4>Title</h4>
-                        <input/>
-                        <h4>Difficulty</h4>
-                        <input/>
-                        <h4>Tags</h4>
-                        <input/>
-                    </form>
-                </div>
-                <div className="instructions-container">
-                    <h3>instructions</h3>
-                    <p>we are going to pull a paragraph from backend and will render here.</p>
-                </div>
-                <div id="markdown">
-
-                </div>
-                <ReactMarkdown source={input} />
-                    
-                <ol>
-                    <li>bold</li>
-                </ol>
-                <div className="creation-editor-container">
-                    <Editor/>
-                    <h1>Hi</h1>
-                </div>
+    
+    return(
+        <div>
+            <div className="meta-container">
+                <h3>Meta</h3>
+                <form>
+                    <h4>Title</h4>
+                    <input/>
+                    <h4>Difficulty</h4>
+                    <input/>
+                    <h4>Tags</h4>
+                    <input/>
+                </form>
             </div>
-        )
-    }
+            <div className="instructions-container">
+                <h3>instructions</h3>
+                <p>we are going to pull a paragraph from backend and will render here.</p>
+            </div>
+            <div className="creation-editor-container">
+                <Editor
+                    input={input}
+                    theme={'material'}
+                    mode={'markdown'}
+                    changeHandler={handleInputChange}
+                />
+                <ReactMarkdown source={input} className="markdown-render"/>
+            </div>
+        </div>
+    )
 }
 
 export default CreateChallenge;
