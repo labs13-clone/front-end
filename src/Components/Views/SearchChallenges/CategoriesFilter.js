67900ce4851734e6d1c737ec999e0ee454ce363a
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-06-14 17:14:52 -0500

    refactored search filters to use API query parameters

--- src/Components/Views/SearchChallenges/CategoriesFilter.js
+++ src/Components/Views/SearchChallenges/CategoriesFilter.js
@@ -3,12 +3,11 @@ import React from 'react';
 
 const CategoriesFilter = (props) => {
 
-  //Todo: Get category names from API
-
   return (
     <div >
-        <select onChange={(e) => props.filterByCategory(e.target.value)}>
-            {props.categories.map(category => <option key={category.id} value={category.name}>{category.name}</option>)}
+        <select onChange={(e) => props.setCategory(e.target.value === "null" ? null : Number(e.target.value))}>
+            <option value={false}>All Categories</option>
+            {props.categories.map(category => <option key={category.id} value={category.id}>{category.name}</option>)}
         </select>
     </div>
   );
--- src/Components/Views/SearchChallenges/DifficultyLevels.js
+++ src/Components/Views/SearchChallenges/DifficultyLevels.js
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
-import jssPluginPropsSort from 'jss-plugin-props-sort';
-
+import React from 'react';
 
 const DifficultyLevels = (props) => {
-
   return (
-    <div >
-        
-        <select onChange={(e) => props.filterByDifficulty(e.target.value)}>
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
+    <div>        
+        <select onChange={(e) => props.setDifficulty(e.target.value)}>
+            <option value="1-100">All Difficulties</option>
+            <option value="1-33">Easy</option>
+            <option value="34-66">Medium</option>
+            <option value="67-100">Hard</option>
         </select>
     </div>
   );
