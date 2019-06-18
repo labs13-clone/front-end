59ac6b9cf0e2dc4338270b700dbfe3dc093d5697
Lidii <lidiiany92@gmail.com>
2019-06-18 17:53:45 -0400

    Added cta and testimonials sections to the landing page

--- src/Components/Views/LandingPage/LandingPageContent.js
+++ src/Components/Views/LandingPage/LandingPageContent.js
@@ -2,6 +2,10 @@ import React from "react";
 import landing1 from '../../../images/landing1.svg';
 import landing2 from '../../../images/landing2.svg';
 import landing3 from '../../../images/landing3.svg';
+import testimonial1 from '../../../images/testimonials1.png';
+import testimonial2 from '../../../images/testimonials2.png';
+import testimonial3 from '../../../images/testimonials3.png';
+import logo from '../../../images/Logo.svg';
 require('./LandingPage.css')
 
 
@@ -14,22 +18,19 @@ const LandingPageContent = props => {
     return (
         <div className="landingContainer">
             <div className="landingContent">
-                <header>
-                    <h1>Clone Coding</h1>
-                    <div>
-                        <button onClick={login}>Sign In / Register</button>
-                    </div>
-                </header>
-                <div className="topContent">
-                    <h2>Achieve mastery through challenge</h2>
-                    <div className="topText">
-                        <p>
-                            Clone Coding is the best platform to help you enhance your skills, expand your
-                            knowledge, and prepare for technical interviews.
-                        </p>
-                        <div className="topButtons">
-                            <button>Continue with Google</button>
-                            <button>Continue with Github</button>
+                <div className="cta">
+                    <header className="header">
+                        <img src={logo} />
+                    </header>
+                    <div className="topContent">
+                        <h2>Achieve mastery through challenge</h2>
+                        <div className="topText">
+                            <p>
+                                Clone Coding is the best platform to help you enhance your skills, expand your
+                                knowledge, and prepare for technical interviews.
+                            </p>
+
+                            <button onClick={login}>Join to Challenge</button>
                         </div>
                     </div>
                 </div>
@@ -61,6 +62,32 @@ const LandingPageContent = props => {
                                 </p>
                             </div>
                             <img src={landing3} />
+                        </div>
+                    </div>
+                </div>
+                <div className="landingContent">
+                    <h3 className="testimonials-header">Testimonials</h3>
+                    <div className="cta testimonials">
+                        <div className="testimonial-card card-sm">
+                            <div>
+                                <img src={testimonial1}/>
+                            </div>
+                            <h3>Eileen Cuevas</h3>
+                            <p>ChallengeJS goes beyond teaching basic syntax and instead teaches its users how to think like programmers. Great start I love the look and content thus far.</p>
+                        </div>
+                        <div className="testimonial-card">
+                            <div>
+                            <img src={testimonial3}/>
+                            </div>
+                            <h3>Kai Lovingfoss</h3>
+                            <p>I am a teacher and I believe coding is an excellent way to teach critical thinking and problem-solving skills. Use this site as a homework, a center, or in a lab setting. Activities are self-paced, so differentiation is easy. However, it is still a good idea, if possible, to seat a more experienced computer user with one who is less experienced. Explain to students that coding is a critical skill in today's world filled with technology and will also be a valuable skill in the other job market.</p>
+                        </div>
+                        <div className="testimonial-card card-sm">
+                        <div>
+                            <img src={testimonial2}/>
+                            </div>
+                            <h3>Nedim Omerovic</h3>
+                            <p>It is my favorite site to practice problems, learned a lot from it.</p>
                         </div>
                     </div>
                 </div>
