c7363608fe48f0dc9166ae2f0b75ed36f749e872
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-06-19 15:44:23 -0500

    converted Callback component to a functional component

--- src/Auth/Callback.js
+++ src/Auth/Callback.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import loading from './loading.svg';
 
-class Callback extends Component {
-  render() {
+
+const Callback = () => {
+
     const style = {
-      position: 'absolute',
-      display: 'flex',
-      justifyContent: 'center',
-      height: '100vh',
-      width: '100vw',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      backgroundColor: 'white',
+        position: 'absolute',
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100vh',
+        width: '100vw',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'white'
     }
 
     return (
-      <div style={style}>
-        <img src={loading} alt="loading"/>
-      </div>
+        <div style={style}>
+            <img src={loading} alt="loading"/>
+        </div>
     );
-  }
 }
 
 export default Callback;
