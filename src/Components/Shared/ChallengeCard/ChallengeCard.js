1d3636a64761788526e4f9edad067296b22d713d
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-06-20 08:21:38 -0500

    added style to approve challenge button

--- src/Components/Shared/ChallengeCard/ChallengeCard.js
+++ src/Components/Shared/ChallengeCard/ChallengeCard.js
@@ -7,7 +7,10 @@ import './ChallengeCard.css';
 
 const ChallengeCard = (props) => {
 
-    const categories = props.challenge.categories.map(cat => <button key={cat.id} className="pill-button">{cat.name}</button>);
+    const categories = props
+        .challenge
+        .categories
+        .map(cat => <button key={cat.id} className="pill-button">{cat.name}</button>);
 
     //Button click handler that approves a challenge
     const approveChallenge = _ => {
@@ -19,38 +22,43 @@ const ChallengeCard = (props) => {
             headers: {
                 Authorization: `Bearer ${props.auth.accessToken}`
             },
-            data: {
-                id: props.challenge.id,
-                approved: 1
-            }
-        })
-        .then(response => {
+                data: {
+                    id: props.challenge.id,
+                    approved: 1
+                }
+            })
+            .then(response => {
             props.challenge = response.data;
         })
-        .catch(err => {
-            console.log(err.message)
-        });
+            .catch(err => {
+                console.log(err.message)
+            });
     }
 
-    //Only show the approve button if the challenge is not approved
-    //AND only show it if the user is an admin
-    const approveButton = (!props.challenge.approved && props.auth.user !== undefined && props.auth.user.role === 'admin') && <button onClick={approveChallenge}>Approve Challenge</button>
-    return (
-        <div onClick={_ => history.replace(`/challenges/${props.challenge.id}`)} className="challenge-wrapper">
+    // Only show the approve button if the challenge is not approved AND only show
+    // it if the user is an admin
+    const approveButton = (!props.challenge.approved && props.auth.user !== undefined && props.auth.user.role === 'admin')
+        ? <button className="approve-challenge-button" onClick={approveChallenge}>Approve Challenge</button>
+        : null;
 
-                <div className="card-body">
-                    <h1 className="challenge-title" >{props.challenge.title}</h1>
-                </div>
+    return (
+        <div
+            onClick={_ => history.replace(`/challenges/${props.challenge.id}`)}
+            className="challenge-wrapper">
+
+            <div className="card-body">
+                <h1 className="challenge-title">{props.challenge.title}</h1>
+            </div>
+            <div>
+                <ReactMarkdown source={props.challenge.description}/>
+            </div>
+            <div className="card-footer">
                 <div>
-                    <ReactMarkdown source={props.challenge.description}/>
+                    {categories}
                 </div>
-                <div className="card-footer">
-                    <div>
-                        {categories}
-                        {approveButton}
-                    </div>
-                    <div className="pill-button" >{difficultyToString(props.challenge.difficulty)}</div>
-                </div>
+                <div className="pill-button">{difficultyToString(props.challenge.difficulty)}</div>
+            </div>
+            {approveButton}
         </div>
     );
 }
