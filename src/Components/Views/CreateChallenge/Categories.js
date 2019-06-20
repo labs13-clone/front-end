63e72d59440f5d63bbe98926dcebc90fbb0394e1
chasefulks <chase.fulks@gmail.com>
2019-06-20 14:25:08 -0500

    landingpagestylesfixed

--- src/Components/Views/CreateChallenge/Categories.js
+++ src/Components/Views/CreateChallenge/Categories.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import MultiSelect from "@khanacademy/react-multi-select";
+
 
 
 
 function CategoryDropDown(props) {
 
-  return(
-    <MultiSelect
-        options={props.options}
-        selected={props.selectedCategories}
-        onSelectedChanged={selected => props.setSelectedCategories(selected)}
-      />
-  ) 
+  // return(
+  //   <MultiSelect
+  //       options={props.options}
+  //       selected={props.selectedCategories}
+  //       onSelectedChanged={selected => props.setSelectedCategories(selected)}
+  //     />
+  // ) 
 }
 
 export default CategoryDropDown;
