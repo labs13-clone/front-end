d240bfb5696ddc0569cd5adeb814a35dcc9862a2
WilliamConnatser <43946230+WilliamConnatser@users.noreply.github.com>
2020-04-17 21:00:23 -0500

    troubleshooting auth0 v1

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,9 +1,7 @@
-import history from '../history';
-import auth0 from 'auth0-js';
-import {
-  AUTH_CONFIG
-} from './auth0-variables';
-import axios from 'axios';
+import history from "../history";
+import auth0 from "auth0-js";
+import { AUTH_CONFIG } from "./auth0-variables";
+import axios from "axios";
 
 export default class Auth {
   accessToken;
@@ -16,13 +14,13 @@ export default class Auth {
     clientID: AUTH_CONFIG.clientId,
     redirectUri: AUTH_CONFIG.callbackUrl,
     audience: AUTH_CONFIG.audience,
-    responseType: 'id_token token',
-    scope: 'openid profile'
+    responseType: "id_token token",
+    scope: "openid profile",
   });
 
   login = () => {
     this.auth0.authorize();
-  }
+  };
 
   handleAuthentication = () => {
     this.auth0.parseHash((err, authResult) => {
@@ -34,36 +32,36 @@ export default class Auth {
       //Else go home
       //If there's an error alert user
       else if (err) {
-        history.replace('/');
+        history.replace("/");
         alert(`Error: ${err.error}. Check the console for further details.`);
       }
     });
-  }
+  };
 
   getAccessToken = () => {
     return this.accessToken;
-  }
+  };
 
   getIdToken = () => {
     return this.idToken;
-  }
+  };
 
   getUser = () => {
     return this.user;
-  }
+  };
 
   setSession = (authResult, path) => {
-
     // Set isLoggedIn flag in localStorage
-    localStorage.setItem('isLoggedIn', 'true');
+    localStorage.setItem("isLoggedIn", "true");
 
     // Set the time that the access token will expire at
-    let expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
+    let expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
     this.accessToken = authResult.accessToken;
     this.idToken = authResult.idToken;
     this.expiresAt = expiresAt;
 
-    if (process.env.NODE_ENV !== 'production') console.log('Your auth0 access token', this.accessToken);
+    if (process.env.NODE_ENV !== "production")
+      console.log("Your auth0 access token", this.accessToken);
 
     // Request the user information from the backend
     this.getUserInfo();
@@ -76,31 +74,30 @@ export default class Auth {
 
     */
 
-    console.log("path: " + path)
+    console.log("path: " + path);
 
     //Most of the time after going through the auth0 lock screen from the landing page
     //The path will be "/callback" so we navigate to the challenges page
-    if (path === "/callback" ||
+    if (
+      path === "/callback" ||
       // || (path=== undefined && localStorage.getItem('isLoggedIn')) was added to stop occasional bug
       // When you clicked on the call to action on landing page you would sometimes be redirected back to landing
       // Even after successful authorization. Turns out path was occasionally undefined.
-      (path === undefined && localStorage.getItem('isLoggedIn'))) {
+      (path === undefined && localStorage.getItem("isLoggedIn"))
+    ) {
       history.replace(`/challenges`);
     } else {
       history.replace(`${path}`);
     }
-
-  }
+  };
 
   renewSession = (path) => {
     this.auth0.checkSession({}, (err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
-
         //Set session
         this.setSession(authResult, path);
-
       } else if (err) {
-        localStorage.clear()
+        localStorage.clear();
         this.logoutForReal();
         // this.auth0.logout({
         //   returnTo: AUTH_CONFIG.returnTo
@@ -109,10 +106,12 @@ export default class Auth {
         //Todo: Possible bug?
         //Occasionally, we were getting this alert endlessly
         //So we added localStorage.clear();
-        alert(`Could not get a new token (${err.error}: ${err.error_description}).`);
+        alert(
+          `Could not get a new token (${err.error}: ${err.error_description}).`
+        );
       }
     });
-  }
+  };
 
   logout = () => {
     // Remove tokens and expiry time
@@ -122,13 +121,13 @@ export default class Auth {
     this.expiresAt = 0;
 
     // Remove isLoggedIn flag from localStorage
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("isLoggedIn");
 
     this.logoutForReal();
     // this.auth0.logout({
     //   returnTo: AUTH_CONFIG.returnTo
     // });
-  }
+  };
 
   isAuthenticated = () => {
     // Check whether the current time is past the
@@ -136,33 +135,33 @@ export default class Auth {
 
     const expiresAt = this.expiresAt;
 
-    return (new Date().getTime() < expiresAt);
-  }
+    return new Date().getTime() < expiresAt;
+  };
 
   logoutForReal = () => {
-    if (process.env.NODE_ENV === 'production') {
-      this.auth0.logout({
-        returnTo: 'https://clone-coding-client.herokuapp.com'
-      });
-    } else {
-      this.auth0.logout();
-    }
-  }
+    // if (process.env.NODE_ENV === 'production') {
+    this.auth0.logout({
+      returnTo: process.env.RETURNTO,
+    });
+    // } else {
+    //   this.auth0.logout();
+    // }
+  };
 
   getUserInfo = () => {
     axios({
-        method: 'get',
-        url: `${process.env.REACT_APP_SERVER}/api/users`,
-        headers: {
-          Authorization: `Bearer ${this.accessToken}`
-        }
-      })
-      .then(response => {
+      method: "get",
+      url: `${process.env.REACT_APP_SERVER}/api/users`,
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`,
+      },
+    })
+      .then((response) => {
         //Set auth0 user profile
         this.user = response.data;
       })
-      .catch(err => {
-        console.log(err.message)
+      .catch((err) => {
+        console.log(err.message);
       });
-  }
+  };
 }
